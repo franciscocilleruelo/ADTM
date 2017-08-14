57557e16fcbfbfdf265a52ca4c3b6e019b6dfed6
FRANCISCO <FRANCISCO@FRAN>
2017-08-14 12:48:23 +0200

    First commit

---
+++ ADTM/src/main/java/es/uned/master/software/tfm/adtm/AdtmApplication.java
@@ -0,0 +1,12 @@
+package es.uned.master.software.tfm.adtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdtmApplication.class, args);
+	}
+}
